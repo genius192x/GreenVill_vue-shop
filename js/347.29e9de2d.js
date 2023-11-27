f8d0da1499807a9f9d3d26689abf3f7f206603fa
Михаил Левченко <misahlevchenko161@gmail.com>
2023-11-27 22:27:35 +0300

    Updates

--- js/347.29e9de2d.js
+++ js/347.29e9de2d.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkgreenvill"]=self["webpackChunkgreenvill"]||[]).push([[347],{6347:function(e,n,r){r.r(n),r.d(n,{default:function(){return t}});var u=r(9269);function i(e,n){return(0,u.wg)(),(0,u.iD)("h1",null,"This is an DelasView page")}var l=r(7617);const a={},s=(0,l.Z)(a,[["render",i]]);var t=s}}]);
-//# sourceMappingURL=347.8fc6b129.js.map
+//# sourceMappingURL=347.29e9de2d.js.map
